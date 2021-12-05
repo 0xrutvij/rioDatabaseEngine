e8663d6fa53bd18585d343551bfd459aba835946
sohamsavalapurkar <soham1999@gmail.com>
2021-12-05 16:29:22 -0600

    insert queries added to internal queries

--- src/utils/internal_queries.py
+++ src/utils/internal_queries.py
@@ -1,29 +1,80 @@
-def internal_parse_dict_cdata(x):
+def internal_parse_dict_cdata(table_name):
     parse_dict = {
         "command": "SELECT",
-        "column_name_list": ["column_name","data_type", "ordinal_position", "is_nullable","column_key"],
+        "column_name_list": [
+            "column_name",
+            "data_type",
+            "ordinal_position", 
+            "is_nullable",
+            "column_key"
+             ],
         "table_name": "riobase_columns",
         "condition": {
         "negated": "FALSE",
         "column_name": "table_name",
         "comparator": "=",
-        "value": x
+        "value": table_name
         }
     }
     return parse_dict
 
-def internal_parse_dict_tdata(x):
+def internal_parse_dict_tdata(table_name):
     parse_dict = {
         "command": "SELECT",
-        "column_name_list": ["page_size", "record_count"],
+        "column_name_list": [
+            "page_size",
+            "record_count"
+         ],
         "table_name": "riobase_tables",
         "condition": {
         "negated": "FALSE",
         "column_name": "table_name",
         "comparator": "=",
-        "value": x
+        "value": table_name
         }
     }
     return parse_dict
+
+def insert_parse_dict_cdata(table_name, column_name, data_type, ordinal_position, is_nullable, column_key):
+    insert_parse_dict = {
+        "command": "INSERT INTO TABLE",
+        "column_name_list": [
+            "table_name",
+            "column_name",
+            "data_type",
+            "ordinal_position", 
+            "is_nullable",
+            "column_key"
+        ],
+        "table_name": "riobase_columns",
+        "value_list": [
+            table_name, 
+            column_name,
+            data_type, 
+            ordinal_position, 
+            is_nullable, 
+            column_key
+        ]
+    }
+    return insert_parse_dict
+
+def insert_parse_dict_tdata(table_name, page_size, record_count):
+    insert_parse_dict = {
+        "command": "INSERT INTO TABLE",
+        "column_name_list": [
+            "table_name",
+            "page_size",
+            "record_count"
+        ],
+        "table_name": "riobase_tables",
+        "value_list": [
+            table_name,
+            page_size,
+            record_count
+        ]
+    }
+    return insert_parse_dict
+
+
 if __name__ == "__main__":
     pass
