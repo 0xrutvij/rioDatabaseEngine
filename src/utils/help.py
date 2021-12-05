2af6453d2a2d33436b62e9cf360212efaaa971b8
sohamsavalapurkar <soham1999@gmail.com>
2021-12-05 14:48:21 -0600

    Help display file added

---
+++ src/utils/help.py
@@ -0,0 +1,12 @@
+def help():
+    help = ["clear  : Clear previous input data",
+            "insert : Insert data into a particular table",
+            "create : create a new table in the database"
+            "select : Query data from the database",
+            "update : Update data in the tables",
+            "delete : Delete data from a table",
+            "drop   : Delete table from database",
+            "exit   : Exit davisbase RioDB"  
+    ]
+    return '\n'.join(help)
+print(help())
